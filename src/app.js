b8542c5636edda21dbafcdb6db12e7623ddfb9a6
Victor <santtosvictor42@gmail.com>
2022-04-08 16:03:59 -0300

    alteraçoes

--- src/app.js
+++ src/app.js
@@ -5,12 +5,33 @@ dotenv.config();
 
 import './database';
 
+import helmet from 'helmet';
+import cors from 'cors';
 import express from 'express';
 import homeRoutes from './routes/home';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/token';
 import alunoRoutes from './routes/alunoRoutes';
 import photosRoutes from './routes/photo';
+
+const whitelist = [
+  'http://localhost:3000',
+];
+
+const corsConfig = {
+  origin(origin, callback) {
+    let corsOptions;
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      // eslint-disable-next-line no-unused-vars
+      corsOptions = { origin: true };
+      callback(null, true);
+    } else {
+      // eslint-disable-next-line no-unused-vars
+      corsOptions = { origin: false };
+      callback(new Error('Cors'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -24,6 +45,8 @@ class App {
     this.app.use(express.json());
     this.app.disable('x-powered-by');
     this.app.use(express.static(resolve(__dirname, 'src', 'uploads')));
+    this.app.use(helmet());
+    this.app.use(cors(corsConfig));
   }
 
   routes() {
